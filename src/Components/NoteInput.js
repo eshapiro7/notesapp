86ff61547d91ec0174c629695054087051a40e1f
Elizabeth Shapiro <elizabethashapiro@gmail.com>
2023-01-15 13:46:17 -0500

    tracking input changes to console

--- src/Components/NoteInput.js
+++ src/Components/NoteInput.js
@@ -1,28 +1,24 @@
 import { useState } from "react";
 
 const NoteInput = () => {
-    const [newNote, setNewNote] = useState("");
+  const [newNote, setNewNote] = useState("");
 
-    const savedNote = (event) => {
-        setNewNote(event.target.value);
-    };
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
 
-    console.log(newNote);
-    console.log(setNewNote);
-    console.log(savedNote);
-
-    return (
-        <div className="noteContainer">
-            <h3>New Note</h3>
-            <input
-                onChange={savedNote}
-                className="inputbox"
-                placeholder="Write your note here..."
-            ></input>
-            <button onClick={savedNote}>Save</button>
-            <p>Here is your previous note: {newNote}</p>
-        </div>
-    );
+  return (
+    <div className="noteContainer">
+      <h3>New Note</h3>
+      <input
+        onChange={handleChange}
+        className="inputbox"
+        placeholder="Write your note here..."
+      ></input>
+      <button>Save</button>
+      <p>Here is your previous note: {newNote}</p>
+    </div>
+  );
 };
 
 export default NoteInput;
